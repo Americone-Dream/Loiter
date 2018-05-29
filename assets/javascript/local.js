ca18689b0699112ba41095ba66cd14ea161ff534
yeungpirate <yeungpirate@gmail.com>
2018-05-29 13:09:41 -0700

    save before pulling from 2juicy

--- assets/javascript/local.js
+++ assets/javascript/local.js
@@ -10,10 +10,6 @@ $(document).ready(function() {
 // VARIABLES
 //========================================================================================================================
     
-    var title = responseLocal.events[i].title;
-    var venueInfo = responseLocal.events[i].venue.name;
-    var venueTime = responseLocal.events[i].datetime_local;
-    venueTime = moment(venueTime).format('LLLL');
 
 //========================================================================================================================
 // FUNCTIONS
@@ -25,17 +21,29 @@ $(document).ready(function() {
 //========================================================================================================================
 
     var queryLocal = 'https://api.seatgeek.com/2/events?geoip=true&client_id=MTE2OTc1MDh8MTUyNzEzODIxMC42Mw'
-        $.ajax({
-            url: queryLocal,
-            method: 'GET'
-        }).then(function(responseLocal) {
-            console.log(responseLocal);
-
-            for (i = 0; i < responseLocal.length; i++) {
-                $('.collapsible').append($('<li>', {id: 'result' + i}));
-                $('.collapsible-header').append($('<i class="material-icons">local_activity</i>'));
-                $('#result' + i).append($('<div>', {class: 'collapsible-header', text: title}));
-                $('#result' + i).append($('<div', {class: 'collapsible-body', text: venueTime + ' - ' + venueInfo}));
-            }
-        })
+    $.ajax({
+        url: queryLocal,
+        method: 'GET'
+    }).then(function(responseLocal) {
+        console.log(responseLocal);
+        
+    for (i = 0; i < responseLocal.length; i++) {
+        
+        var title = responseLocal.events[i].title;
+        var venueInfo = responseLocal.events[i].venue.name;
+        var venueTime = responseLocal.events[i].datetime_local;
+        venueTime = moment(venueTime).format('LLLL');
+        console.log(title);
+        console.log(vneueInfo);
+        console.log(venueTime);
+        
+        if (i == 0) {
+            // $('.collapsible').append($('<li>', {id: 'result0'}));
+            $('#headerResult0').append('test');
+            // $('.collapsible-header').append($('<i class="material-icons">local_activity</i>'));
+            $('#bodyResult0').append('TEST');
+            
+        }
+    }
+    })
 })
